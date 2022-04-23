717cfb2b07b2a6acc82cfa29a7a60023aa70fa5f
amelialcruz <95726314+amelialcruz@users.noreply.github.com>
2022-04-23 04:49:49 -0500

    Add files via upload
    Problem 1 – Output your name
    
    Use the Windows Desktop Wizard or Xcode to create a new C++ Console Application named Exercise1.
    
    Add a line of code that will display your name to the user.
    
    Problem 2 – Output your best friend's or your nemesis' name
    
    Add code to output the name of your best friend or your nemesis.

---
+++ Exercise1.cpp
@@ -0,0 +1,26 @@
+// Copyright A. Cruz. All Rights Reserved.
+//
+
+#include <iostream>
+
+/**
+ * Problem 1 - Prints name
+ * Problem 2 - Prints best friend's name
+ * @return Exit status
+*/
+int main()
+{
+    std::cout << "My name is Amelia\n";
+    std::cout << "My best friend's name is Arturo\n";
+}
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
